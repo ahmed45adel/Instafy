9c6e696f30e3f9f9c21666313986fd9ecebbaad6
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-24 12:40:01 +0300

    create layout for auth

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,26 @@
-const AuthLayout = () => {
-  return (
-    <div>AuthLayout</div>
-  )
-}
+import { Outlet, Navigate } from "react-router-dom";
 
-export default AuthLayout
+
+export default function AuthLayout() {
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/images/side-img.jpeg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
+  );
+}
