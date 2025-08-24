9c5fa4422b3f7d92c38ba5aee08564c9a82abfba
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-24 12:17:04 +0300

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import './globals.css';
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SignupForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
 
 const App = () => {
   return (
-     <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <main className="flex min-h-screen">
+    <Routes>
+      {/* public */}
+      <Route element={<AuthLayout />}>
+        <Route index element={<SigninForm />} />
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+      </Route>
+      {/* private */}
+      <Route element={<RootLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  </main>
   )
 }
 
