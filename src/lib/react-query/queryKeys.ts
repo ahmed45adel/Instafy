0f9b11c005598a56b9f96282c7bd0d580076eead
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-27 11:54:48 +0300

    query keys for posts

--- src/lib/react-query/queryKeys.ts
+++ src/lib/react-query/queryKeys.ts
@@ -1,6 +1,8 @@
 export enum QUERY_KEYS {
-  // AUTH KEYS
   CREATE_USER_ACCOUNT = "createUserAccount",
+  GET_CURRENT_USER = "getCurrentUser",
   GET_RECENT_POSTS = "getRecentPosts",
-
+  GET_POSTS = "getPosts",
+  GET_POST_BY_ID = "getPostById",
+  GET_USER_POSTS = "getUserPosts",
 }
