00fb4a457a65f257444df79f1b6e6e3cee10877f
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-27 11:36:36 +0300

    post name and date with location

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -25,6 +25,23 @@ const PostCard = ({ post }: PostCardProps) => {
               className="w-12 lg:h-12 rounded-full"
             />
           </Link>
+
+          <div className="flex flex-col">
+            <p className="base-medium lg:body-bold text-light-1">
+              {post.creator.name}
+            </p>
+            <div className="flex-center gap-2 text-light-3">
+              <p className="subtle-semibold lg:small-regular ">
+                {multiFormatDateString(post.$createdAt)}
+              </p>
+              •
+              <p className="subtle-semibold lg:small-regular">
+                {post.location}
+              </p>
+            </div>
+          </div>
+        </div>
+
         <Link
           to={`/update-post/${post.$id}`}
           className={`${user.id !== post.creator.$id && "hidden"}`}>
