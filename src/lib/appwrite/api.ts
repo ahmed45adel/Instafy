c6fa6118243517a6c9b938ccd2791f32baf9ad71
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-28 12:36:53 +0300

    get user posts api

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -375,3 +375,21 @@ export async function getPostById(postId?: string) {
     console.log(error);
   }
 }
+
+export async function getUserPosts(userId?: string) {
+  if (!userId) return;
+
+  try {
+    const post = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      [Query.equal("creator", userId), Query.orderDesc("$createdAt")]
+    );
+
+    if (!post) throw Error;
+
+    return post;
+  } catch (error) {
+    console.log(error);
+  }
+}
