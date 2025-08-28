9f60ea7b312f5003b23928a778a5e8d800658f91
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-28 12:54:09 +0300

    organize types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,22 +1,18 @@
-export type INewUser = {
-  name: string;
-  email: string;
-  username: string;
-  password: string;
-};
-export type IUser = {
-  id: string;
-  name: string;
-  username: string;
-  email: string;
-  imageUrl: string;
-  bio: string;
-};
 export type INavLink = {
   imgURL: string;
   route: string;
   label: string;
 };
+
+export type IUpdateUser = {
+  userId: string;
+  name: string;
+  bio: string;
+  imageId: string;
+  imageUrl: URL | string;
+  file: File[];
+};
+
 export type INewPost = {
   userId: string;
   caption: string;
@@ -24,6 +20,7 @@ export type INewPost = {
   location?: string;
   tags?: string;
 };
+
 export type IUpdatePost = {
   postId: string;
   caption: string;
@@ -32,4 +29,20 @@ export type IUpdatePost = {
   file: File[];
   location?: string;
   tags?: string;
+};
+
+export type IUser = {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  imageUrl: string;
+  bio: string;
+};
+
+export type INewUser = {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
 };
