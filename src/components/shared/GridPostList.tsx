bdaba554e30d3c4b66c2e5893f48f966f69cfd1c
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-28 12:48:21 +0300

    post list ui with basic func

---
+++ src/components/shared/GridPostList.tsx
@@ -0,0 +1,31 @@
+import { Models } from "appwrite";
+import { Link } from "react-router-dom";
+
+type GridPostListProps = {
+  posts: Models.Document[];
+  showUser?: boolean;
+  showStats?: boolean;
+};
+
+const GridPostList = ({
+  posts
+}: GridPostListProps) => {
+
+  return (
+    <ul className="grid-container">
+      {posts.map((post) => (
+        <li key={post.$id} className="relative min-w-80 h-80">
+          <Link to={`/posts/${post.$id}`} className="grid-post_link">
+            <img
+              src={post.imageUrl}
+              alt="post"
+              className="h-full w-full object-cover"
+            />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default GridPostList;
