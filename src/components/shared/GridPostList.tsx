6a85c3bf3fc1c82d7a9196e9346c832cfabe7c39
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-28 12:50:41 +0300

    show user details in post list

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,6 +1,9 @@
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
 
+import { PostStats } from "@/components/shared";
+import { useUserContext } from "@/context/AuthContext";
+
 type GridPostListProps = {
   posts: Models.Document[];
   showUser?: boolean;
@@ -8,8 +11,11 @@ type GridPostListProps = {
 };
 
 const GridPostList = ({
-  posts
+  posts,
+  showUser = true,
+  showStats = true,
 }: GridPostListProps) => {
+  const { user } = useUserContext();
 
   return (
     <ul className="grid-container">
@@ -22,6 +28,23 @@ const GridPostList = ({
               className="h-full w-full object-cover"
             />
           </Link>
+
+          <div className="grid-post_user">
+            {showUser && (
+              <div className="flex items-center justify-start gap-2 flex-1">
+                <img
+                  src={
+                    post.creator.imageUrl ||
+                    "/assets/icons/profile-placeholder.svg"
+                  }
+                  alt="creator"
+                  className="w-8 h-8 rounded-full"
+                />
+                <p className="line-clamp-1">{post.creator.name}</p>
+              </div>
+            )}
+            {showStats && <PostStats post={post} userId={user.id} />}
+          </div>
         </li>
       ))}
     </ul>
