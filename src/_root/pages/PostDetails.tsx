0896cddc440cb27687f55994a24a63faad06f9d0
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-28 12:46:27 +0300

    related posts

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -128,6 +128,14 @@ const PostDetails = () => {
           </div>
         </div>
       )}
+
+      <div className="w-full max-w-5xl">
+        <hr className="border w-full border-dark-4/80" />
+
+        <h3 className="body-bold md:h3-bold w-full my-10">
+          More Related Posts
+        </h3>
+      </div>
     </div>
   );
 };
