394e6878163bec710edf5def6d0fb4a8c71c9d18
Ahmed Adel <ahmed45adel@yahoo.com>
2025-08-31 19:36:20 +0300

    search posts in explore page

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,7 +1,60 @@
-const Explore = () => {
-  return (
-    <div>Explore</div>
-  )
-}
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
-export default Explore
+import { Input } from "@/components/ui";
+import { Loader } from "@/components/shared";
+import { useGetPosts } from "@/lib/react-query/queries";
+
+export type SearchResultProps = {
+  isSearchFetching: boolean;
+  searchedPosts: any;
+};
+
+
+const Explore = () => {
+  const { inView } = useInView();
+  const { data: posts, fetchNextPage } = useGetPosts();
+
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    if (inView && !searchValue) {
+      fetchNextPage();
+    }
+  }, [inView, searchValue]);
+
+  if (!posts)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+
+  return (
+    <div className="explore-container">
+      <div className="explore-inner_container">
+        <h2 className="h3-bold md:h2-bold w-full">Search Posts</h2>
+        <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
+          <img
+            src="/assets/icons/search.svg"
+            width={24}
+            height={24}
+            alt="search"
+          />
+          <Input
+            type="text"
+            placeholder="Search"
+            className="explore-search"
+            value={searchValue}
+            onChange={(e) => {
+              const { value } = e.target;
+              setSearchValue(value);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
