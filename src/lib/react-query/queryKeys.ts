c6c68a5efcd853abdfdb918211505336254f8827
Ahmed Adel <ahmed45adel@yahoo.com>
2025-09-01 17:18:27 +0300

    add query keys

--- src/lib/react-query/queryKeys.ts
+++ src/lib/react-query/queryKeys.ts
@@ -1,11 +1,20 @@
 export enum QUERY_KEYS {
+  // AUTH
   CREATE_USER_ACCOUNT = "createUserAccount",
+
+  // USER
   GET_CURRENT_USER = "getCurrentUser",
-  GET_RECENT_POSTS = "getRecentPosts",
+  GET_USERS = "getUsers",
+  GET_USER_BY_ID = "getUserById",
+
+  // POST
   GET_POSTS = "getPosts",
+  GET_INFINITE_POSTS = "getInfinitePosts",
+  GET_RECENT_POSTS = "getRecentPosts",
   GET_POST_BY_ID = "getPostById",
   GET_USER_POSTS = "getUserPosts",
-  GET_USER_BY_ID = "getUserById",
-  GET_INFINITE_POSTS = "getInfinitePosts",
+  GET_FILE_PREVIEW = "getFilePreview",
+
+  //  SEARCH
   SEARCH_POSTS = "getSearchPosts",
 }
