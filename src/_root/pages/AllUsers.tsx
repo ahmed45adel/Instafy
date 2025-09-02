4c626e187051efd6064eeff39a8775530408e745
Ahmed Adel <ahmed45adel@yahoo.com>
2025-09-02 14:36:20 +0300

    display all users

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,7 +1,28 @@
-const AllUsers = () => {
-  return (
-    <div>AllUsers</div>
-  )
-}
+import { Loader, UserCard } from "@/components/shared";
+import { useGetUsers } from "@/lib/react-query/queries";
 
-export default AllUsers
+const AllUsers = () => {
+
+  const { data: creators } = useGetUsers();
+
+  return (
+    <div className="common-container">
+      <div className="user-container">
+        <h2 className="h3-bold md:h2-bold text-left w-full">All Users</h2>
+        {!creators ? (
+          <Loader />
+        ) : (
+          <ul className="user-grid">
+            {creators?.documents.map((creator) => (
+              <li key={creator?.$id} className="flex-1 min-w-[200px] w-full  ">
+                <UserCard user={creator} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AllUsers;
