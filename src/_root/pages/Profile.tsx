fbd944bafb41e3cdbc9bc910050d3ebc399e9baa
Ahmed Adel <ahmed45adel@yahoo.com>
2025-09-02 14:55:49 +0300

    profile page ui

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -1,7 +1,53 @@
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+import {
+  Route,
+  Routes,
+  Outlet,
+  useParams,
+} from "react-router-dom";
+import { LikedPosts } from "@/_root/pages";
+import { useUserContext } from "@/context/AuthContext";
+import { useGetUserById } from "@/lib/react-query/queries";
+import { GridPostList, Loader } from "@/components/shared";
 
-export default Profile
+
+const Profile = () => {
+  const { id } = useParams();
+  const { user } = useUserContext();
+
+  const { data: currentUser } = useGetUserById(id || "");
+
+  if (!currentUser)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+
+  return (
+    <div className="profile-container">
+      <div className="profile-inner_container">
+        <div className="flex xl:flex-row flex-col max-xl:items-center flex-1 gap-7">
+          <img
+            src={
+              currentUser.imageUrl || "/assets/icons/profile-placeholder.svg"
+            }
+            alt="profile"
+            className="w-28 h-28 lg:h-36 lg:w-36 rounded-full"
+          />
+        </div>
+      </div>
+      <Routes>
+        <Route
+          index
+          element={<GridPostList posts={currentUser.posts} showUser={false} />}
+        />
+        {currentUser.$id === user.id && (
+          <Route path="/liked-posts" element={<LikedPosts />} />
+        )}
+      </Routes>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Profile;
