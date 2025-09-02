f0a5e4cbcb9a8dceeb5e9a01eaf6a480f33e60bd
Ahmed Adel <ahmed45adel@yahoo.com>
2025-09-02 15:01:59 +0300

    update profile ui

---
+++ src/_root/pages/UpdateProfile.tsx
@@ -0,0 +1,95 @@
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useParams } from "react-router-dom";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { ProfileUploader } from "@/components/shared";
+import { ProfileValidation } from "@/lib/validation";
+import { useUserContext } from "@/context/AuthContext";
+import { useGetUserById, useUpdateUser } from "@/lib/react-query/queries";
+
+const UpdateProfile = () => {
+  const { id } = useParams();
+  const { user, setUser } = useUserContext();
+  const form = useForm<z.infer<typeof ProfileValidation>>({
+    resolver: zodResolver(ProfileValidation),
+    defaultValues: {
+      file: [],
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      bio: user.bio || "",
+    },
+  });
+
+  // Queries
+  const { data: currentUser } = useGetUserById(id || "");
+  const { mutateAsync: updateUser } =
+    useUpdateUser();
+
+  // Handler
+  const handleUpdate = async (value: z.infer<typeof ProfileValidation>) => {
+    const updatedUser = await updateUser({
+      userId: currentUser.$id,
+      name: value.name,
+      bio: value.bio,
+      file: value.file,
+      imageUrl: currentUser.imageUrl,
+      imageId: currentUser.imageId,
+    });
+    setUser({
+      ...user,
+      name: updatedUser?.name,
+      bio: updatedUser?.bio,
+      imageUrl: updatedUser?.imageUrl,
+    });
+  };
+
+  return (
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="flex-start gap-3 justify-start w-full max-w-5xl">
+          <img
+            src="/assets/icons/edit.svg"
+            width={36}
+            height={36}
+            alt="edit"
+            className="invert-white"
+          />
+          <h2 className="h3-bold md:h2-bold text-left w-full">Edit Profile</h2>
+        </div>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleUpdate)}
+            className="flex flex-col gap-7 w-full mt-4 max-w-5xl">
+            <FormField
+              control={form.control}
+              name="file"
+              render={({ field }) => (
+                <FormItem className="flex">
+                  <FormControl>
+                    <ProfileUploader
+                      fieldChange={field.onChange}
+                      mediaUrl={currentUser.imageUrl}
+                    />
+                  </FormControl>
+                  <FormMessage className="shad-form_message" />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateProfile;
