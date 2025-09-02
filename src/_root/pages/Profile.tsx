b982922519091226469070509739b9e4c82a844c
Ahmed Adel <ahmed45adel@yahoo.com>
2025-09-02 15:01:32 +0300

    view followers and following stats

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -13,6 +13,17 @@ import { useUserContext } from "@/context/AuthContext";
 import { useGetUserById } from "@/lib/react-query/queries";
 import { GridPostList, Loader } from "@/components/shared";
 
+interface StatBlockProps {
+  value: string | number;
+  label: string;
+}
+// blocks for viewing following, followers stats
+const StatBlock = ({ value, label }: StatBlockProps) => (
+  <div className="flex-center gap-2">
+    <p className="small-semibold lg:body-bold text-primary-500">{value}</p>
+    <p className="small-medium lg:base-medium text-light-2">{label}</p>
+  </div>
+);
 
 const Profile = () => {
   const { id } = useParams();
@@ -48,6 +59,13 @@ const Profile = () => {
                 @{currentUser.username}
               </p>
             </div>
+
+            <div className="flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20">
+              <StatBlock value={currentUser.posts.length} label="Posts" />
+              <StatBlock value={20} label="Followers" />
+              <StatBlock value={20} label="Following" />
+            </div>
+
             <p className="small-medium md:base-medium text-center xl:text-left mt-7 max-w-screen-sm">
               {currentUser.bio}
             </p>
